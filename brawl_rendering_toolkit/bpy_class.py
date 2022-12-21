033e91b93bf6b0fd83396aca987ea1f405d300cb
tryptech <yo.tryptech@gmail.com>
2022-12-20 19:19:55 -0500

    Modularize panel

--- brawl_rendering_toolkit/bpy_class.py
+++ brawl_rendering_toolkit/bpy_class.py
@@ -6,4 +6,12 @@ from . import ui
 classes = [
     modules.brawlImport.OBJECT_OT_brawlcrate_collada_import,
     modules.brawlImport.POSE_OT_brawlcrate_anim_import,
+    ui.panel.POSE_ARMATURE_PT_brt_panel,
+    ui.panel.ARMATURES_PT_panel,
+    ui.panel.IMPORT_PT_panel,
+    ui.panel.POSING_PT_panel,
+    ui.panel.RENDER_PT_panel,
+    ui.panel.UTILITY_PT_panel,
+    ui.panel.BLOP_PT_rigui_CSPUR,
+    ui.panel.BLOP_PT_customprops_CSPUR
 ]
---
+++ brawl_rendering_toolkit/ui/panel.py
@@ -0,0 +1,306 @@
+import bpy
+
+from ..operators import brtOps 
+
+#-----------------------------------------
+
+class POSE_ARMATURE_PT_brt_panel(bpy.types.Panel):
+    bl_idname = "POSE_ARMATURE_PT_brt_panel"
+    bl_label = "Brawl Rendering Toolkit"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Brawl"
+
+    
+    def draw(self,context):
+        
+        '''
+        user:readme: Since the animation rig's pose identity is equal to world identity, 
+        the user can't just do Alt-R/G/S to get back to the T-Pose. The operators below
+        are the recreated functionality. (3DView->Tools->Animation->BrawlCrate->Clear)
+        '''
+
+class ARMATURES_PT_panel(bpy.types.Panel): 
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Brawl"
+    bl_parent_id = "POSE_ARMATURE_PT_brt_panel"
+    bl_options = {"DEFAULT_CLOSED"}
+    bl_idname = "ARMATURES_PT_panel"
+    bl_label = "Armatures"
+    
+    def draw(self, context):
+        layout = self.layout.column(align=True)
+        scene = bpy.context.scene
+
+        row = layout.column(align = True)
+        row.prop_search(
+            scene, 'proxy', bpy.data, "objects", text='Proxy', icon='ARMATURE_DATA' 
+        )
+
+class IMPORT_PT_panel(bpy.types.Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Brawl"
+    bl_parent_id = "POSE_ARMATURE_PT_brt_panel"
+    bl_options = {"DEFAULT_CLOSED"}
+    bl_idname = "IMPORT_PT_panel"
+    bl_label = "Import"
+    
+    def draw(self, context):
+        layout = self.layout.column(align=True)
+        scene = bpy.context.scene
+        
+        row = layout.row(align=True)
+        op = row.operator(OBJECT_OT_brawlcrate_collada_import.bl_idname,text='DAE')
+        op = row.operator(POSE_OT_brawlcrate_anim_import.bl_idname,text='ANIM')
+        
+        layout.row().separator()
+        layout.row().separator()
+        row = layout.column(align=True)
+        op = row.operator(DATA_OT_brt_init_polish_setup.bl_idname,text='Setup for Manual Polish',icon='SPHERE')
+        op = row.operator(OBJECT_OT_brt_set_object_mods.bl_idname,text='Apply Default Modfiers',icon='MODIFIER')
+        
+        layout.row().separator()
+        layout.row().separator()
+        row = layout.column(align=True)
+        op = row.operator(POSE_ARMATURE_OT_config_ik.bl_idname,text='Configure IK',icon='POSE_HLT')
+        op = row.operator(POSE_ARMATURE_OT_bind_ik.bl_idname,text='Bind IK Rig',icon='CON_ARMATURE')
+
+class POSING_PT_panel(bpy.types.Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Brawl"
+    bl_parent_id = "POSE_ARMATURE_PT_brt_panel"
+    bl_options = {"DEFAULT_CLOSED"}
+    bl_idname = "POSING_PT_panel"
+    bl_label = "Posing"
+    
+    def draw(self, context):
+        layout = self.layout.column(align=True)
+        scene = bpy.context.scene
+        
+        column = layout.column(align=True)
+        op = column.operator(OBJECT_OT_brt_toggle_proxy.bl_idname,text='Toggle Proxy')
+        op = column.operator(POSE_ARMATURE_OT_clear_to_bind.bl_idname,text='Reset Proxy')
+        op.clear_location=True
+        op.clear_rotation=True
+        op.clear_scale=True
+
+class RENDER_PT_panel(bpy.types.Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Brawl"
+    bl_parent_id = "POSE_ARMATURE_PT_brt_panel"
+    bl_options = {"DEFAULT_CLOSED"}
+    bl_idname = "RENDER_PT_panel"
+    bl_label = "Render"
+
+    def draw(self, context):
+        layout = self.layout.column(align=True)
+        scene = bpy.context.scene
+        rs = scene.RBTab_Settings
+        anim_render = rs.switchStillAnim_prop
+        saveInLocalFolder = rs.saveInBlendFolder
+
+        if bpy.data.is_saved == False:
+            layout.use_property_split = False
+            row = layout.row(align=True)
+            row.alignment='CENTER'
+            row.alert = True
+            row.label(text=' Save Blend File First  --->', icon='INFO')
+            row.operator('wm.save_mainfile', text='', icon='FILE_TICK')
+            row.alert = False
+
+        if hasattr(bpy.types, "MYBIGBUTTONTAB_PT_MyBigButton"):
+            if bpy.data.is_saved == False:
+                layout.use_property_split = False
+                row = layout.row(align=True)
+                row.alignment='CENTER'
+                row.alert = True
+                row.label(text=' Save Blend File First  --->', icon='INFO')
+                row.operator('wm.save_mainfile', text='', icon='FILE_TICK')
+                row.alert = False
+            else:
+                row = layout.row(align=True)
+
+                row.prop(rs,'saveInBlendFolder',text='Save in blend folder' if saveInLocalFolder else 'Save in custom path',icon='BLENDER')
+
+                row = layout.row(align=True)
+
+                if saveInLocalFolder == False:
+                    row.prop(scene.render, "filepath", text="")
+                    row = layout.row(align=True)
+
+                layout.row().separator()
+                layout.row().separator()
+                row = layout.row(align=True)
+
+                row.prop(scene, "frame_float" if scene.show_subframe else "frame_current", text="Current")
+                row.prop(rs, "switchStillAnim_prop", text="",icon='RENDER_ANIMATION')
+
+                if anim_render:
+                    row = layout.row(align=True)
+
+                    row.prop(scene, "use_preview_range", text="", toggle=True)
+
+                    sub = row.row(align=True)
+
+                    sub.prop(scene, "frame_preview_start" if scene.use_preview_range else "frame_start", text="Start")
+                    sub.prop(scene, "frame_preview_end" if scene.use_preview_range else "frame_end", text="End")
+
+                layout.row().separator()
+                layout.row().separator()
+                row = layout.column(align=True)
+
+                if anim_render:
+                    row.operator(IMAGE_OT_reload_and_render_anim.bl_idname,text="Render Current Camera w/ Recolors", icon='OUTLINER_OB_IMAGE')
+                    row.operator(IMAGE_OT_reload_and_render_all_anim.bl_idname,text="Render All Cameras w/ Recolors", icon='RENDER_RESULT')
+                else:
+                    row.operator(IMAGE_OT_reload_and_render.bl_idname,text="Render Current Camera", icon='OUTLINER_OB_IMAGE')
+                    row.operator(IMAGE_OT_reload_and_render_all.bl_idname,text="Render All Cameras", icon='RENDER_RESULT')
+
+class UTILITY_PT_panel(bpy.types.Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Brawl"
+    bl_parent_id = "POSE_ARMATURE_PT_brt_panel"
+    bl_options = {"DEFAULT_CLOSED"}
+    bl_idname = "UTILITY_PT_panel"
+    bl_label = "Utility"
+    
+    def draw(self, context):
+        layout = self.layout.column(align=True)
+        scene = bpy.context.scene
+
+        row = layout.column(align=True)
+        op = row.operator(DATA_OT_brt_purge.bl_idname,text='Clean Unused Data',icon='TRASH') 
+        op = row.operator(IMAGE_OT_reload_textures.bl_idname,text='Reload Textures',icon='FILE_REFRESH')
+
+class BLOP_PT_rigui_CSPUR(bpy.types.Panel):
+	bl_space_type = 'VIEW_3D'
+	bl_region_type = 'UI'
+	bl_category = 'Brawl'
+	bl_label = "CSPUR UI"
+	bl_idname = "BLOP_PT_rigui_CSPUR"
+
+	@classmethod
+	def poll(self, context):
+		try:
+			return (context.active_object.data.get("blm_rig_id") == blm_rig_id)
+		except (AttributeError, KeyError, TypeError):
+			return False
+
+	def draw(self, context):
+		layout = self.layout
+		col = layout.column()
+
+
+		row = col.row(align=True)
+		row.prop(context.active_object.data,'layers', index=8, toggle=True, text='Center')
+
+		row = col.row(align=True)
+		row.prop(context.active_object.data,'layers', index=9, toggle=True, text='Right Arm')
+		row.prop(context.active_object.data,'layers', index=12, toggle=True, text='Left Arm')
+
+		row = col.row(align=True)
+		row.prop(context.active_object.data,'layers', index=10, toggle=True, text='Right Hand')
+		row.prop(context.active_object.data,'layers', index=13, toggle=True, text='Left Hand')
+
+		row = col.row(align=True)
+		row.prop(context.active_object.data,'layers', index=11, toggle=True, text='Right Foot')
+		row.prop(context.active_object.data,'layers', index=14, toggle=True, text='Left Foot')
+
+		row = col.row(align=True)
+		row.prop(context.active_object.data,'layers', index=15, toggle=True, text='Fine Controls')
+
+		row = col.row(align=True)
+		row.prop(context.active_object.data,'layers', index=0, toggle=True, text='Config')
+		row.prop(context.active_object.data,'layers', index=2, toggle=True, text='Apply')
+
+		row = col.row(align=True)
+		row.prop(context.active_object.data,'layers', index=30, toggle=True, text='Deform Tweak')
+
+class BLOP_PT_customprops_CSPUR(bpy.types.Panel):
+	bl_category = 'Brawl'
+	bl_label = "CSPUR Properties"
+	bl_idname = "BLOP_PT_customprops_CSPUR"
+	bl_space_type = 'VIEW_3D'
+	bl_region_type = 'UI'
+	bl_options = {'DEFAULT_CLOSED'}
+
+	@classmethod
+	def poll(self, context):
+		if context.active_object.data.get('blm_rig_id'):
+			pose_bones = context.selected_pose_bones
+			props = None
+			rna_properties = {prop.identifier for prop in bpy.types.PoseBone.bl_rna.properties if prop.is_runtime}
+			if context.selected_pose_bones:
+				bones = context.selected_pose_bones
+
+			elif context.selected_editable_bones:
+				bones = [pose_bones[bone.name] for bone in context.selected_editable_bones]
+
+			elif context.mode == 'OBJECT':
+				bones = context.active_object.pose.bones
+
+			else:
+				return False
+			if bones:
+				props = [[prop for prop in bone.items() if prop not in rna_properties] for bone in bones]
+
+			if props and bones:
+				return (context.active_object.data.get("blm_rig_id") == blm_rig_id)
+			else:
+				return False
+
+		else:
+			return False
+
+	def draw(self, context):
+		layout = self.layout
+		pose_bones = context.active_object.pose.bones
+		if context.selected_pose_bones:
+			bones = context.selected_pose_bones
+
+		elif context.selected_editable_bones:
+			bones = [pose_bones[bone.name] for bone in context.selected_editable_bones]
+
+		else:
+			bones = context.active_object.pose.bones
+
+		def assign_props(row, val, key):
+			row.property = key
+			row.data_path = "active_pose_bone"
+			try:
+				row.value = str(val)
+			except:
+				pass
+
+		rna_properties = {
+			prop.identifier for prop in bpy.types.PoseBone.bl_rna.properties
+			if prop.is_runtime
+		}
+
+	# make scripts backwards compatible
+		skip = 0
+		skip_keys = rna_properties
+		if bpy.app.version < (3, 0, 0):
+			skip_keys = rna_properties.union({"_RNA_UI"})
+			skip = 1
+	# Iterate through selected bones add each prop property of each bone to the panel.
+
+		for bone in context.selected_pose_bones:
+			if len(bone.keys()) > skip:
+				box = layout.box()
+			for key in sorted(bone.keys()):
+				if key not in skip_keys:
+					val = bone.get(key, "value")
+					row = box.row()
+					split = row.split(align=True, factor=0.7)
+					row = split.row(align=True)
+					row.label(text=key, translate=False)
+					row = split.row(align=True)
+					row.prop(bone, f'["{key}"]', text = "", slider=True)
+
+#-----------------------------------------
